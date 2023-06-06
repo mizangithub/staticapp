1bfeefd103513d286099946e8a39cc62756fc945
Mizanur Rahman <mizanur.rahman@cityelectricsupply.com>
2023-06-05 23:44:03 -0500

    local test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  const value = 'World::: This is a test...';
+  const value = 'World::: This is a test :: in a local pc...';
   return <div>Hello {value}</div>;
 }
 
