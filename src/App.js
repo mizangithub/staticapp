993b426acdfa7d589a027923b55c828fbe7c5b5f
Mizanur Rahman <mizanur.rahman@cityelectricsupply.com>
2023-06-06 16:56:56 -0500

    api testing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 function App() {
-  const [data, setData] = useState('');
+  
+  //const [data, setData] = useState('');
+  
+ // const data = "Test...data ... ";
+
+
+  const [ loading, setLoading ] = useState(true);
+  const [data, setData] = useState([])
 
   useEffect(() => {
-    (async function () {
-      const { text } = await( await fetch(`/api/testapi`)).json();
-      setData(text);
-    })();
-  });
+
+    const fetchData = async () =>{
+      setLoading(true);
+
+      try {
+      
+        const {data: response} = await axios.get('/api/testapi');
+
+        console.log("This is a test.. api call..");
+
+        setData(response);
+
+      } 
+      catch (error) {
+        console.error(error.message);
+      }
+      
+      setLoading(false);
+    }
+
+    fetchData();
+  }, []);
+
+
+
+  
 
   return <div>
-    <h4>Test App with integrated api: {data}</h4>
-    
-    </div>;
+            <h4>Test App with integrated api: {data}</h4>    
+         </div>;
 }
 
 export default App;
